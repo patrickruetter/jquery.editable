156cac7c73c40ce60de797d9db8c252f0f0732ee
Patrick Rütter <patrickruetter@gmail.com>
2014-11-29 14:56:46 +0100

    initial commit

---
+++ js/jquery.editable.js
@@ -0,0 +1,113 @@
+/*!
+ * Adds inline editing functionality
+ *
+ * Authors: Patrick Rütter, Martin Haas
+ *
+ * Plugin structure based on jQuery Boilerplate's Basic Plugin Boilerplate
+ * (https://github.com/jquery-boilerplate/jquery-patterns/blob/master/patterns/jquery.basic.plugin-boilerplate.js)
+ */
+
+;
+(function ($, window, document, undefined) {
+
+    var pluginName = "editable",
+        defaults = {
+            onSave: function (element) {
+            },
+            onCancel: function (element) {
+            }
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+
+        this.options = $.extend({}, defaults, options);
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+        currentHtml: "",
+        init: function () {
+            var editable = this;
+            var $editableElement = $(this.element);
+
+            $editableElement.click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                // save current content
+                var html = $.trim($editableElement.html());
+                this.currentHtml = html;
+
+                // add editable controls
+                var $editableControls = $('<div class="editable-controls"></div>');
+
+                $editableElement.after($editableControls);
+
+                var $textArea = $("<textarea>")
+                    .val(html)
+                    .width("100%")
+                    .height($editableElement.height())
+                    .keyup(function (event) {
+                        if (e.keyCode == 27) { // escape key
+                            editable.cancelHandler(event, editable);
+                        }
+                    });
+                var $okButton = $("<button>")
+                    .text("OK")
+                    .addClass("editable-ok")
+                    .click(function (event) {
+                        editable.okHandler(event, editable);
+                    });
+                var $cancelButton = $("<button>")
+                    .text("Cancel")
+                    .addClass("editable-cancel")
+                    .click(function (event) {
+                        editable.cancelHandler(event, editable);
+                    });
+
+                $editableControls
+                    .append($textArea)
+                    .append($okButton)
+                    .append($cancelButton);
+
+                $editableElement.hide();
+
+            });
+        },
+
+        okHandler: function (event, editable) {
+            var $editableElement = $(editable.element);
+            var $editableControls = $editableElement.next('.editable-controls');
+            var $editableNewContent = $editableControls.find('textarea');
+
+            editable.currentHtml = $editableNewContent.val();
+            $editableElement.html(editable.currentHtml);
+            $editableControls.remove();
+            $editableElement.show();
+        },
+
+        cancelHandler: function (event, editable) {
+            var $editableElement = $(editable.element);
+            var $editableControls = $editableElement.next('.editable-controls');
+
+            $editableElement.show();
+            $editableControls.remove();
+        }
+
+    };
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                    new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
